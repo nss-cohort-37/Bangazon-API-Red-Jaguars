6d63756cc2c3fd55e9a4f445981418bda727e337
jwgilliam <ggsmoustache@gmail.com>
2020-04-01 16:01:17 -0500

    merge conflicts

---
+++ BangazonAPI/Models/Orders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonAPI.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int UserPaymentTypesId {get; set;}
+    }
+}
